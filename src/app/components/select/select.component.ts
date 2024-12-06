1f1a26f939693db65d61d1325707a4ea301f3ae2
Francisco de Assis Abreu Miranda <dassishot@gmail.com>
2024-12-06 19:21:32 +0000

    Commit em 2024-12-06 07-21 PM

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,32 +1,49 @@
-import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, computed, HostListener, signal } from '@angular/core';
 
 @Component({
 	selector: 'app-select',
-	imports: [NgFor],
+	imports: [NgFor, NgIf],
 	templateUrl: './select.component.html'
 })
 export class SelectComponent {
-	options = ['Banana', 'Maçã', 'Laranja', 'Abacaxi', 'Uva', 'Pera', 'Melancia']
-	filteredOptions = [...this.options]
-	searchTerm = '';
-	selectedOption: string | null = null;
-	isDropdownOpen = false;
-  
-	toggleDropdown(open: boolean): void {
-	  this.isDropdownOpen = open;
+	protected options = signal<string[]>(['Banana', 'Maçã', 'Laranja', 'Abacaxi', 'Uva', 'Pera', 'Melancia'])
+
+	protected isDropdownOpen = signal<boolean>(false)
+	protected showAdd = signal<boolean>(false)
+	protected selectedOptions = signal<string[]>([])
+	protected filteredOptions = signal<string[]>([...this.options()])
+
+	availableOptions = computed(() =>
+		this.options().filter(option => !this.selectedOptions().includes(option)))
+
+	toggleDropdown(): void {
+		this.isDropdownOpen.set(!this.isDropdownOpen())
 	}
-  
-	filterOptions(): void {
-	  const term = this.searchTerm.toLowerCase();
-	  this.filteredOptions = this.options.filter(option =>
-		option.toLowerCase().includes(term)
-	  );
+
+	filterOptions(query: any): void {
+		const lowerCaseQuery = query.target.value.toLowerCase()
+		const matches = this.options().filter(option => option.toLowerCase().includes(lowerCaseQuery))
+		this.filteredOptions.set(matches)
+		this.showAdd.set(matches.length === 0);
 	}
-  
+
 	selectOption(option: string): void {
-	  this.selectedOption = option;
-	  this.searchTerm = option;
-	  this.isDropdownOpen = false;
+		if (!this.selectedOptions().includes(option)) {
+			this.selectedOptions.set([...this.selectedOptions(), option])
+		}
+		this.isDropdownOpen.set(false)
+		this.showAdd.set(false)
+		this.filteredOptions.set(this.availableOptions())
+	}
+
+	removeOption(option: string): void {
+		this.selectedOptions.set(
+			this.selectedOptions().filter(selected => selected !== option)
+		)
+		this.filteredOptions.set(this.availableOptions())
+	}
+
+	onBlur(event: FocusEvent): void {
 	}
 }
