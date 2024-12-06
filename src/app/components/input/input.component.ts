ec6685948091e029c094cbef282e6c743c06f10b
Francisco de Assis Abreu Miranda <dassishot@gmail.com>
2024-12-06 01:50:03 +0000

    Commit em 2024-12-06 01-50 AM

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common'
+import { AfterContentInit, Component, computed, ContentChild, Input, signal } from '@angular/core'
+import { NgModel } from '@angular/forms'
 
 @Component({
-	selector: 'app-input',
-	imports: [],
+	selector: '[app-input]',
+	imports: [NgIf],
 	templateUrl: './input.component.html'
 })
-export class InputComponent {
+export class InputComponent implements AfterContentInit {
+	@Input({ required: true }) title!: string
 	@Input({ required: true }) label!: string
 	@Input({ required: true }) errorMessage!: string
+
+	@ContentChild(NgModel) model!: NgModel
+
+	protected ipt: NgModel | undefined = undefined
+
+	ngAfterContentInit(): void {
+		this.ipt = this.model
+		if(this.ipt === undefined) {
+			throw new Error('This component needs the ngModel directive');
+		}
+	}
 }
--- src/app/views/home/create/create.component.ts
+++ src/app/views/home/create/create.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { ModalComponent } from '../../../components/modal/modal.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from '../../../components/input/input.component';
 
 @Component({
   selector: 'app-create',
-  imports: [ModalComponent],
+  imports: [FormsModule, CommonModule, ModalComponent, InputComponent],
   templateUrl: './create.component.html'
 })
 export class CreateComponent {
